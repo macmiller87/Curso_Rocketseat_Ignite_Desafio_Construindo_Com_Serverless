132eca91851fd1a210570e54a1e2a5e7e3d40bb0
Macmiller <macamagolf@gmail.com>
2022-05-26 00:18:54 -0300

    Criando estrutura do projeto

---
+++ serverless.ts
@@ -0,0 +1,98 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'curso-rocketseat-ignite-desafio-construindo-com-serverless',
+  frameworkVersion: '3',
+  plugins: [
+    'serverless-esbuild', 
+    'serverless-dynamodb-local',
+    'serverless-offline'
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    lambdaHashingVersion: '20201221',
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["dynamodb:*"],
+        Resource: ["*"]
+      }
+    ]
+  },
+
+  // import the function via paths
+  functions: {
+    createTodo: {
+      handler: "src/functions/createTodo.handle",
+      events: [
+        {
+          http: {
+            path: "createTodo/{user_id}",
+            method: "post",
+            cors: true
+          }
+        }
+      ]
+    }
+  },
+
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+    dynamodb: {
+      stages: ["dev", "local"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true
+      },
+    },
+  },
+
+  resources: {
+    Resources: {
+      dbTodos: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "usersTodo",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "user_id",
+              AttributeType: "S"
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: "user_id",
+              KeyType: "HASH"
+            }
+          ]
+        }
+      }
+    }
+  }
+};
+
+module.exports = serverlessConfiguration;
